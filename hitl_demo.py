8ab3c66acf864f13e365df015972ec7c1a545a11
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-16 14:55:39 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -11,24 +11,16 @@ import streamlit as st
 path = os.path.dirname(__file__)
 
 # load csv's
-unlabeled = pd.read_csv(path +'/unlabeled.csv')
-labeled = pd.read_csv(path +'/labeled.csv')
+unlabeled = pd.read_csv(path + '/unlabeled.csv')
+labeled = pd.read_csv(path + '/labeled.csv')
 
-# split unlabeled data into to batches to feed into model after labeling
+# split unlabeled data into batches to feed into the model after labeling
 batch1 = unlabeled[:50].copy()
 batch2 = unlabeled[50:].copy()
 
-# split labeled data into to batches to feed into model
+# split labeled data into batches to feed into the model
 batch3 = labeled[:50].copy()
 batch4 = labeled[50:].copy()
-
-# load model
-model = tf.keras.models.load_model(path + "/heart_disease_model.h5")
-
-# load test data
-loaded_data = np.load(path + "/test_data.npz")
-X_test = loaded_data["X_test"]
-y_test = loaded_data["y_test"]
 
 # global df variable for labeling results
 df = None
@@ -69,11 +61,14 @@ def load_latest(path):
     return latest
 
 def main():
-    
     st.title("Heart Disease Model Active Learning Demo App")
     score = {}
     s = 0
     data = []
+    
+    if "labeled_data" not in st.session_state:
+        st.session_state.labeled_data = {}  # Initialize the labeled data in session_state
+
     for i, j in enumerate([batch1, batch2]):
         if s == 0:  # if in the first loop
             # Load the model
@@ -85,25 +80,27 @@ def main():
         
         # label new data
         if st.button("Label Data", key=f"label_button_{i}"):
-            global df
-            df = add_labels(j)
+            labeled_data = add_labels(j)
+            st.session_state.labeled_data[f"batch_{i}"] = labeled_data
         
         # retrain
         if st.button("Retrain Model", key=f"retrain_button_{i}"):
             st.text("Retraining Model...")  # Display a message indicating retraining
-            X = df.drop("target", axis=1)
-            y = df["target"]
-            loaded_model.fit(X, y, epochs=10, batch_size=32)
-            
-            # evaluate (you should provide X_test and y_test)
-            loss, acc = loaded_model.evaluate(X_test, y_test)
-            score[i] = acc
-            st.text(f"Model accuracy after retraining: {acc:.2f}")
-            
-            # update model
-            fingerprint = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            new_name = "/heart_disease_model" + "_" + fingerprint + ".h5"
-            loaded_model.save(path + new_name)
+            labeled_data = st.session_state.labeled_data.get(f"batch_{i}")
+            if labeled_data is not None:
+                X = labeled_data.drop("target", axis=1)
+                y = labeled_data["target"]
+                loaded_model.fit(X, y, epochs=10, batch_size=32)
+                
+                # evaluate (you should provide X_test and y_test)
+                loss, acc = loaded_model.evaluate(X_test, y_test)
+                score[i] = acc
+                st.text(f"Model accuracy after retraining: {acc:.2f}")
+                
+                # update model
+                fingerprint = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                new_name = "/heart_disease_model" + "_" + fingerprint + ".h5"
+                loaded_model.save(path + new_name)
 
         if st.button("Plot Experiment", key=f"plot_button_{i}"):
             data = [0.83] + list(score.values())
