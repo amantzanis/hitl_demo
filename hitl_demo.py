2ace7e4d93dd89f833196440c31925e17cee13bc
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-16 17:55:30 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -30,24 +30,30 @@ y_test = loaded_data["y_test"]
 # global df variable for labeling results
 df = None
 
-def add_labels(df):
+    def add_labels(df):
     l = []
+    current_instance = 0  # Track the current instance
 
-    for i in range(len(df)):
+    while current_instance < len(df):
         st.text("Data Instance:")
-        st.write(df.iloc[i])  # Display the data instance
+        st.write(df.iloc[current_instance])  # Display the data instance
         st.text("Please label this instance either 1 or 0")
 
         # Generate a unique key based on the loop index
-        input_key = f"label_{i}"
+        input_key = f"label_{current_instance}"
 
         # Use a Streamlit number_input widget with a unique key
         x = st.number_input("Label", min_value=0, max_value=1, step=1, key=input_key)
         l.append(x)
 
+        # Add a "Next" button to move to the next instance
+        if st.button("Next"):
+            current_instance += 1
+
     df["target"] = pd.Series(l)
 
     return df
+
 
 def load_latest(path):
     date_format = "%Y-%m-%d %H:%M:%S"
