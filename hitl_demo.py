c0cb79b80607533d87dacc19f400b7d9dcdd5281
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-18 16:23:29 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -9,42 +9,28 @@ data = {
     "Target": [0 for i in range(20)]  # Initialize with 0s
 }
 
+df = pd.DataFrame(data)
+
 # Set up the Streamlit layout
 st.title("Data Annotation App")
 
-# Create a container to hold the DataFrame
-container = st.container()
+# Create a two-column layout
+col1, col2 = st.beta_columns([2, 1])
 
-# Use st.sidebar() for the selection on the left side
-with st.sidebar():
+# Display the DataFrame in the first column (col1)
+with col1:
+    st.write("Data to Annotate:")
+    st.dataframe(df)
+
+# Create a form to update the target labels in the second column (col2)
+with col2:
     st.write("Instructions: Please label the 'Target' column with 0 or 1.")
     for index, row in df.iterrows():
         st.write(f"Instance {index + 1}:")
         label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
         df.at[index, 'Target'] = label
 
-# Use CSS to position the DataFrame on the right and make it sticky
-st.markdown(
-    """
-    <style>
-    .main {
-        display: flex;
-    }
+# Show the updated DataFrame
+st.write("Updated DataFrame:")
+st.dataframe(df)
 
-    .block-container {
-        flex: 1;
-    }
-
-    .block-container > div {
-        position: sticky;
-        top: 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# Display the DataFrame within the container
-with container:
-    st.markdown("Data to Annotate:")
-    st.dataframe(df)
