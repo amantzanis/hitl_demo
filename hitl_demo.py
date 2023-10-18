36f376a51e9692789f53be297d49802b3e18d56a
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-18 16:18:40 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -12,17 +12,22 @@ df = pd.DataFrame(data)
 
 st.title("Data Annotation App")
 
-# Display the DataFrame in Streamlit
-st.write(df)
+# Create a two-column layout
+col1, col2 = st.beta_columns([2, 1])
 
-st.write("Instructions: Please label the 'Target' column with 0 or 1.")
+# Display the DataFrame in the first column (col1)
+with col1:
+    st.write("Data to Annotate:")
+    st.dataframe(df)
 
-# Create a form to update the target labels
-for index, row in df.iterrows():
-    st.write(f"Instance {index + 1}:")
-    label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
-    df.at[index, 'Target'] = label
+# Create a form to update the target labels in the second column (col2)
+with col2:
+    st.write("Instructions: Please label the 'Target' column with 0 or 1.")
+    for index, row in df.iterrows():
+        st.write(f"Instance {index + 1}:")
+        label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
+        df.at[index, 'Target'] = label
 
-# Show the updated DataFrame
-st.write("Updated DataFrame:")
-st.write(df)
+    # Show the updated DataFrame
+    st.write("Updated DataFrame:")
+    st.dataframe(df)
