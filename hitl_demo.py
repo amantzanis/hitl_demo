62ee14eefd244e5c703329e5c8af4e6fb1b6d6b0
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-18 17:02:42 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -12,16 +12,26 @@ df = pd.DataFrame(data)
 
 st.title("Data Annotation App")
 
-# Use st.sidebar for the labeling instances
-with st.sidebar:
-    st.write("Instructions: Please label the 'Target' column with 0 or 1.")
-    for index, row in df.iterrows():
-        st.write(f"Instance {index + 1}:")
-        label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
-        if df.at[index, 'Target'] != label:
-            # Highlight the updated cell with a different background color
-            st.markdown(f'<style>table tr:nth-child({index + 1}) td:nth-child(5){{background-color: blue;}}</style>', unsafe_allow_html=True)
-        df.at[index, 'Target'] = label
+# Create a button to show the sidebar and labeling instances
+relabel_button = st.button("Relabel")
+
+# Initialize a flag to control visibility
+show_sidebar = False
+
+if relabel_button:
+    show_sidebar = True
+
+if show_sidebar:
+    # Use st.sidebar for the labeling instances
+    with st.sidebar:
+        st.write("Instructions: Please label the 'Target' column with 0 or 1.")
+        for index, row in df.iterrows():
+            st.write(f"Instance {index + 1}:")
+            label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
+            if df.at[index, 'Target'] != label:
+                # Highlight the updated cell with a different background color
+                st.markdown(f'<style>table tr:nth-child({index + 1}) td:nth-child(5){{background-color: blue;}}</style>', unsafe_allow_html=True)
+            df.at[index, 'Target'] = label
 
 # Use st.expander to display the original DataFrame
 with st.expander("Original Data", expanded=True):
