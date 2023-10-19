f1a7f5b7f36d979b46facaec7fc2fb860db132a5
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-19 11:52:34 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -21,7 +21,7 @@ st.write(
     <style>
         .stApp {{
             background-image: url("data:image/jpeg;base64,{background_image.data}");
-            background-size: 50px;
+            background-size: 20px;
             background-position: top left;
             background-repeat: no-repeat;
         }}
