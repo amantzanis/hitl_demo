5c353b3c3352c648346da55b812f35af53e7d8e4
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-19 11:04:36 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -45,71 +45,32 @@ with st.expander("Explain", expanded=False):
     image_path = path + '/mygraph.jpg'
     image = st.image(image_path, use_column_width=True)
 
-# st.title("Relabel data here:")
-
-# # Create a checkbox to show/hide the labeling instances
-# show_sidebar = st.checkbox("Show Labeling Instances")
-
-# if show_sidebar:
-#     # Use st.sidebar for the labeling instances
-#     with st.sidebar:
-#         st.write("Instructions: Please label the 'Target' column with 0 or 1.")
-#         for index, row in df.iterrows():
-#             st.write(f"Instance {index}:")
-#             label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
-#             if df.at[index, 'Target'] != label:
-#                 # Highlight the updated cell with a different background color
-#                 st.markdown(f'<style>table tr:nth-child({index + 1}) td:nth-child(5){{background-color: blue;}}</style>', unsafe_allow_html=True)
-#             df.at[index, 'Target'] = label
-
-# # Use st.expander to display the original DataFrame
-# with st.expander("Original Data", expanded=False):
-#     st.write("Data to Annotate:")
-#     st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
-
-# # Use st.expander to display the updated DataFrame
-# with st.expander("Updated Data", expanded=False):
-#     st.write("Updated DataFrame:")
-#     st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
-
-# Set up the Streamlit layout
 st.title("Relabel data here:")
 
 # Create a checkbox to show/hide the labeling instances
 show_sidebar = st.checkbox("Show Labeling Instances")
-
-# Initialize a list to store highlighted rows
-highlighted_rows = []
 
 if show_sidebar:
     # Use st.sidebar for the labeling instances
     with st.sidebar:
         st.write("Instructions: Please label the 'Target' column with 0 or 1.")
         for index, row in df.iterrows():
-            st.write(f"Instance {index + 1}:")
+            st.write(f"Instance {index}:")
             label = st.radio(f"Label Target (0 or 1) for instance {index + 1}:", [0, 1])
-            df.at[index, 'Target'] = label
-
             if df.at[index, 'Target'] != label:
-                # Add the index to the list of highlighted rows
-                highlighted_rows.append(index)
-
-# Create a function to apply highlighting to the DataFrame
-def highlight_rows(row):
-    if row.name in highlighted_rows:
-        return ['background-color: blue'] * len(row)
-    else:
-        return [''] * len(row)
+                # Highlight the updated cell with a different background color
+                st.markdown(f'<style>table tr:nth-child({index + 1}) td:nth-child(5){{background-color: blue;}}</style>', unsafe_allow_html=True)
+            df.at[index, 'Target'] = label
 
 # Use st.expander to display the original DataFrame
 with st.expander("Original Data", expanded=False):
     st.write("Data to Annotate:")
-    st.dataframe(df.style.apply(highlight_rows, axis=1))
+    st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
 
 # Use st.expander to display the updated DataFrame
 with st.expander("Updated Data", expanded=False):
     st.write("Updated DataFrame:")
-    st.dataframe(df.style.apply(highlight_rows, axis=1))
+    st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
 
 st.title("Retrain:")
 # with st.expander("Retrain Model", expanded=False):
