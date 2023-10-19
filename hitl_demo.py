1b06d89047582cd5e19a3ac385a7769a49e30b5e
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-19 11:45:35 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -77,17 +77,3 @@ st.title("Retrain:")
 # with st.expander("Retrain Model", expanded=False):
            
 st.title("Track Metrics:")
-
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-   .sidebar .sidebar-content {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
