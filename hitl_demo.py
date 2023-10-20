8d9cd59cf0391df9d8f30e192a31e17e1ed51e83
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-20 17:54:56 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -126,7 +126,7 @@ if show_sidebar:
         accuracy_values = [initial_accuracy, accuracy_wo_hf, accuracy_new]
         ax.bar(metrics, accuracy_values)
         ax.set_ylabel('Accuracy')
-        ax.tick_params(axis='x', rotation=45, labelsize = 10)
+        ax.tick_params(axis='x', rotation=45, labelsize = 9)
         
         # Annotate the bars with percentages
         for i, v in enumerate(accuracy_values):
