9a9061fe779ad24bd735362e0a7a7aa4732eeb25
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-24 12:26:06 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -144,16 +144,18 @@ if show_sidebar:
             _, accuracy_new = loaded_model1.evaluate(X_test, y_test)
         plot_accuracy_bar(accuracy, accuracy_wo_hf, accuracy_new)
 
-        # Create buttons to choose which model to keep
-    if model_retrained:
-        model_choice = st.radio("Choose a Model to Keep:", ["Original Model", "Retrained Model", "Retrained Model with Human Feedback"])
-    
-        if model_choice == "Original Model":
-            st.write("You have chosen to keep the original model.")
-            # Add code here to save the original model if needed.
-        elif model_choice == "Retrained Model":
-            st.write("You have chosen to keep the retrained model without human feedback.")
-            # Add code here to save the retrained model without human feedback if needed.
-        elif model_choice == "Retrained Model with Human Feedback":
-            st.write("You have chosen to keep the retrained model with human feedback.")
-            # Add code here to save the retrained model with human feedback if needed.
+        st.title("Update Model:")
+        with st.expander("Choose a model:", expanded=False):
+            # Create buttons to choose which model to keep
+        if model_retrained:
+            model_choice = st.radio("Choose a Model to Keep:", ["Original Model", "Retrained Model", "Retrained Model with Human Feedback"])
+        
+            if model_choice == "Original Model":
+                st.write("You have chosen to keep the original model.")
+                # Add code here to save the original model if needed.
+            elif model_choice == "Retrained Model":
+                st.write("You have chosen to keep the retrained model without human feedback.")
+                # Add code here to save the retrained model without human feedback if needed.
+            elif model_choice == "Retrained Model with Human Feedback":
+                st.write("You have chosen to keep the retrained model with human feedback.")
+                # Add code here to save the retrained model with human feedback if needed.
