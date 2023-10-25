c2cafe02c27d31aaf8cc1eb90376ce4fca90c917
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-25 11:42:07 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
 
 # ignore streamlit warnings
 st.set_option('deprecation.showPyplotGlobalUse', False)
