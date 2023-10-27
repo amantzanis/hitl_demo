5154679f6848eca7b1dd8cdb34e60fdee02c1dc0
amantzanis <144322082+amantzanis@users.noreply.github.com>
2023-10-27 16:00:52 +0300

    Update hitl_demo.py

--- hitl_demo.py
+++ hitl_demo.py
@@ -143,7 +143,7 @@ if show_sidebar:
         else:
             _, accuracy_wo_hf = loaded_model.evaluate(X_test, y_test)
             _, accuracy_new = loaded_model1.evaluate(X_test, y_test)
-        plot_accuracy_bar(accuracy, 0.7071, 0.7569)
+        plot_accuracy_bar(accuracy, accuracy_wo_hf, accuracy_new)
 
         if st.button("Keep Initial Model"):
             st.success("Initial model saved successfully!")
